8ed90e30c0da3983f51511291d74a402f33f3da3
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-07-06 18:03:50 +0100

    added utils folder

---
+++ lib/utils/slide_transition.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MySlide extends MaterialPageRoute {
+  MySlide({
+    WidgetBuilder builder,
+    RouteSettings settings,
+  }) : super(builder: builder, settings: settings);
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    Animation<Offset> custom =
+        Tween<Offset>(begin: Offset(1.0, 0.0), end: Offset(0.0, 0.0))
+            .animate(animation);
+    return SlideTransition(
+      position: custom,
+      child: child,
+    );
+    // return super.buildTransitions(context, animation, secondaryAnimation, child);
+  }
+}
