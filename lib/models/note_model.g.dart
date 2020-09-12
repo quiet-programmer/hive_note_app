d665af108abf0ba0aed288b274c8479227f263f1
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-09-12 20:12:14 +0100

    updated generated file

--- lib/models/note_model.g.dart
+++ lib/models/note_model.g.dart
@@ -9,23 +9,36 @@ part of 'note_model.dart';
 class NoteModelAdapter extends TypeAdapter<NoteModel> {
   @override
   final typeId = 0;
-  
+
   @override
   NoteModel read(BinaryReader reader) {
-    var numOfFields = reader.readByte();
-    var fields = <int, dynamic>{
-      for (var i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
     return NoteModel(
       notes: fields[0] as String,
+      myStyle: fields[1] as dynamic,
     );
   }
 
   @override
   void write(BinaryWriter writer, NoteModel obj) {
     writer
-      ..writeByte(1)
+      ..writeByte(2)
       ..writeByte(0)
-      ..write(obj.notes);
+      ..write(obj.notes)
+      ..writeByte(1)
+      ..write(obj.myStyle);
   }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is NoteModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
 }
