aa9acaec07b5d1fa8872ba4bb17235f89359b8b2
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-10-17 08:54:14 +0100

    removed deprecated code

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
-import 'package:note_app/const_value.dart';
+import 'package:note_app/const_values.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/src/app.dart';
 import 'package:path_provider/path_provider.dart';
