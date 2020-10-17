1484c6b0d81071342fdb70254d17af95b10381eb
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-10-17 08:53:22 +0100

    little UI chnages

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,23 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:note_app/screens/home.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
+    // final NoteModel changeTheme = Hive.box(noteBox).get(appHiveKey);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
         appBarTheme: AppBarTheme(
           elevation: 0.0,
+          shadowColor: Colors.transparent,
+          color: Colors.transparent,
           textTheme: TextTheme(
             headline6: TextStyle(
+              color: Colors.black54,
               fontSize: 25.0,
             ),
+          ),
+          iconTheme: IconThemeData(
+            color: Colors.black54,
           ),
         ),
       ),
