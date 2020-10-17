8815a0553fc49f6a322e5dbe603ec62e3193d4db
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-10-17 08:52:55 +0100

    added new values

---
+++ lib/const_values.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+const Color backColor = Color.fromRGBO(224, 211, 175, 1);
+
+const Color appbarColor = Color.fromRGBO(48, 48, 48, 1);
+const Color bodyColor = Color.fromRGBO(33, 33, 33, 1);
+
+
+// Hive details
+const String noteBox = 'notebox';
+const String appHiveKey = "state";
