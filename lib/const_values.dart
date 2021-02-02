cb36d49a5e82b61efc403014517157a8f03e6d84
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-02-02 13:42:51 +0100

    added new values

--- lib/const_values.dart
+++ lib/const_values.dart
@@ -5,7 +5,9 @@ const Color backColor = Color.fromRGBO(224, 211, 175, 1);
 const Color appbarColor = Color.fromRGBO(48, 48, 48, 1);
 const Color bodyColor = Color.fromRGBO(33, 33, 33, 1);
 
-
 // Hive details
 const String noteBox = 'notebox';
 const String appHiveKey = "state";
+
+const String appCast =
+    "https://condescending-euler-c20d1c.netlify.app/index.xml";
