b052cb3d079a55a385922f833befa25da9ca3f68
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-02-02 13:39:49 +0100

    littl ui change

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,6 +7,7 @@ import 'package:note_app/models/note_model.dart';
 import 'package:note_app/screens/note_edit_screen.dart';
 import 'package:note_app/screens/note_screen.dart';
 import 'package:note_app/utils/slide_transition.dart';
+import 'package:upgrader/upgrader.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -25,11 +26,15 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     // final NoteModel changeTheme = Hive.box(noteBox).get(appHiveKey);
+    Upgrader().clearSavedSettings();
+    final config = AppcastConfiguration(
+      url: appCast,
+      supportedOS: ['android'],
+    );
     return Scaffold(
       backgroundColor: backColor,
       appBar: AppBar(
         title: Text("Notes"),
-        centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -44,114 +49,122 @@ class _HomeState extends State<Home> {
           color: Colors.white,
         ),
       ),
-      body: storeData.isEmpty
-          ? Center(
-              child: Text(
-                "No Notes Yet...",
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.black54,
+      body: UpgradeAlert(
+        appcastConfig: config,
+        debugLogging: true,
+        countryCode: "NG",
+        dialogStyle: UpgradeDialogStyle.material,
+        canDismissDialog: true,
+        durationToAlertAgain: Duration(days: 1),
+        child: storeData.isEmpty
+            ? Center(
+                child: Text(
+                  "No Notes Yet...",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black54,
+                  ),
                 ),
-              ),
-            )
-          : SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10.0),
-                child: ValueListenableBuilder(
-                  valueListenable: storeData.listenable(),
-                  builder: (context, Box<NoteModel> notes, _) {
-                    List<int> keys = notes.keys.cast<int>().toList();
-                    return StaggeredGridView.countBuilder(
-                      physics: NeverScrollableScrollPhysics(),
-                      primary: false,
-                      shrinkWrap: true,
-                      crossAxisCount: 4,
-                      mainAxisSpacing: 8.0,
-                      crossAxisSpacing: 8.0,
-                      addRepaintBoundaries: true,
-                      itemBuilder: (_, index) {
-                        final key = keys[index];
-                        final NoteModel note = notes.get(key);
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).push(MySlide(builder: (_) {
-                              return NoteScreen(
-                                note: note,
-                                notekey: key,
-                              );
-                            }));
-                          },
-                          onLongPress: () {
-                            showDialog(
-                              context: context,
-                              builder: (_) {
-                                return AlertDialog(
-                                  backgroundColor: backColor,
-                                  titleTextStyle: TextStyle(
-                                    color: Colors.black54,
-                                  ),
-                                  contentTextStyle: TextStyle(
-                                    color: Colors.black54,
-                                  ),
-                                  title: Text("Warning"),
-                                  content: Text(
-                                      "Are you sure you want to delete this note?"),
-                                  actions: <Widget>[
-                                    FlatButton(
-                                      onPressed: () {
-                                        storeData.delete(key);
-                                        Navigator.of(context).pop();
-                                        setState(() {});
-                                      },
-                                      child: Text(
-                                        "Yes",
-                                        style: TextStyle(
-                                          color: Colors.black54,
-                                        ),
-                                      ),
-                                    ),
-                                    FlatButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: Text(
-                                        "No",
-                                        style: TextStyle(
-                                          color: Colors.black54,
-                                        ),
-                                      ),
-                                    )
-                                  ],
+              )
+            : SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: ValueListenableBuilder(
+                    valueListenable: storeData.listenable(),
+                    builder: (context, Box<NoteModel> notes, _) {
+                      List<int> keys = notes.keys.cast<int>().toList();
+                      return StaggeredGridView.countBuilder(
+                        physics: NeverScrollableScrollPhysics(),
+                        primary: false,
+                        shrinkWrap: true,
+                        crossAxisCount: 4,
+                        mainAxisSpacing: 8.0,
+                        crossAxisSpacing: 8.0,
+                        addRepaintBoundaries: true,
+                        itemBuilder: (_, index) {
+                          final key = keys[index];
+                          final NoteModel note = notes.get(key);
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(MySlide(builder: (_) {
+                                return NoteScreen(
+                                  note: note,
+                                  notekey: key,
                                 );
-                              },
-                            );
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: Colors.white38,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10.0))),
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Text(
-                                note.notes,
-                                style: TextStyle(
-                                  color: Colors.black,
+                              }));
+                            },
+                            onLongPress: () {
+                              showDialog(
+                                context: context,
+                                builder: (_) {
+                                  return AlertDialog(
+                                    backgroundColor: backColor,
+                                    titleTextStyle: TextStyle(
+                                      color: Colors.black54,
+                                    ),
+                                    contentTextStyle: TextStyle(
+                                      color: Colors.black54,
+                                    ),
+                                    title: Text("Warning"),
+                                    content: Text(
+                                        "Are you sure you want to delete this note?"),
+                                    actions: <Widget>[
+                                      FlatButton(
+                                        onPressed: () {
+                                          storeData.delete(key);
+                                          Navigator.of(context).pop();
+                                          setState(() {});
+                                        },
+                                        child: Text(
+                                          "Yes",
+                                          style: TextStyle(
+                                            color: Colors.black54,
+                                          ),
+                                        ),
+                                      ),
+                                      FlatButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text(
+                                          "No",
+                                          style: TextStyle(
+                                            color: Colors.black54,
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.white38,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10.0))),
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(
+                                  note.notes,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                  ),
+                                  softWrap: true,
                                 ),
-                                softWrap: true,
                               ),
                             ),
-                          ),
-                        );
-                      },
-                      itemCount: keys.length,
-                      staggeredTileBuilder: (int index) =>
-                          StaggeredTile.count(2, index.isEven ? 2 : 1),
-                    );
-                  },
+                          );
+                        },
+                        itemCount: keys.length,
+                        staggeredTileBuilder: (int index) =>
+                            StaggeredTile.count(2, index.isEven ? 2 : 1),
+                      );
+                    },
+                  ),
                 ),
               ),
-            ),
+      ),
     );
   }
 }
