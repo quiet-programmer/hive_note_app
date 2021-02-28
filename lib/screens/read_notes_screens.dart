5ab946c407fdc5fe1e68c298c30e22c259de6867
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-02-28 10:44:17 +0100

    added functions to handle text to speech in note

--- lib/screens/read_notes_screens.dart
+++ lib/screens/read_notes_screens.dart
@@ -1,24 +1,164 @@
+import 'dart:io' show Platform;
+
 import 'package:flutter/material.dart';
 import 'package:note_app/const_values.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/screens/edit_note_screen.dart';
 import 'package:note_app/utils/slide_transition.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class ReadNotesScreen extends StatefulWidget {
   final NoteModel note;
-  final int notekey;
+  final int noteKey;
 
   ReadNotesScreen({
     Key key,
     @required this.note,
-    @required this.notekey,
+    @required this.noteKey,
   }) : super(key: key);
 
   @override
   _ReadNotesScreenState createState() => _ReadNotesScreenState();
 }
 
+enum TtsState {
+  playing,
+  stopped,
+  paused,
+  continued,
+}
+
 class _ReadNotesScreenState extends State<ReadNotesScreen> {
+  FlutterTts flutterTts;
+  dynamic languages;
+  String language;
+  double volume = 0.5;
+  double pitch = 1.0;
+  double rate = 0.9;
+
+  String _newVoiceText;
+
+  TtsState ttsState = TtsState.stopped;
+
+  bool get isIOS => !kIsWeb && Platform.isIOS;
+  bool get isAndroid => !kIsWeb && Platform.isAndroid;
+  bool get isWeb => kIsWeb;
+
+  @override
+  void initState() {
+    super.initState();
+    initTts();
+    _onChange(widget.note.notes.toString());
+  }
+
+  void initTts() {
+    flutterTts = FlutterTts();
+
+    _getLanguages();
+
+    // flutterTts.setVoice("en-us-x-sfg#male_1-local");
+    flutterTts.setLanguage("en-Us");
+
+    if (isAndroid) {
+      _getEngines();
+    }
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        print("Playing");
+        ttsState = TtsState.playing;
+      });
+    });
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        print("Complete");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setCancelHandler(() {
+      setState(() {
+        print("Cancel");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    if (isWeb || isIOS) {
+      flutterTts.setPauseHandler(() {
+        setState(() {
+          print("Paused");
+          ttsState = TtsState.paused;
+        });
+      });
+
+      flutterTts.setContinueHandler(() {
+        setState(() {
+          print("Continued");
+          ttsState = TtsState.continued;
+        });
+      });
+    }
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        print("error: $msg");
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future _getLanguages() async {
+    languages = await flutterTts.getLanguages;
+    if (languages != null) setState(() => languages);
+  }
+
+  Future _getEngines() async {
+    var engines = await flutterTts.getEngines;
+    if (engines != null) {
+      for (dynamic engine in engines) {
+        print(engine);
+      }
+    }
+  }
+
+  Future _speak() async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(pitch);
+
+    if (_newVoiceText != null) {
+      if (_newVoiceText.isNotEmpty) {
+        await flutterTts.awaitSpeakCompletion(true);
+        await flutterTts.speak(_newVoiceText);
+      }
+    }
+  }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+  Future _pause() async {
+    var result =
+        await flutterTts.setSilence(widget.note.notes.toString().length);
+    if (result == 1) setState(() => ttsState = TtsState.paused);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+  }
+
+  void _onChange(String text) {
+    setState(() {
+      _newVoiceText = text;
+    });
+  }
+
   final bool isEditing = true;
 
   Widget showText() {
@@ -55,7 +195,7 @@ class _ReadNotesScreenState extends State<ReadNotesScreen> {
               Navigator.of(context).push(MySlide(builder: (_) {
                 return EditNoteScreen(
                   notes: widget.note,
-                  noteKey: widget.notekey,
+                  noteKey: widget.noteKey,
                 );
               }));
             },
@@ -89,6 +229,19 @@ class _ReadNotesScreenState extends State<ReadNotesScreen> {
             ],
           ),
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          ttsState == TtsState.stopped
+              ? Icons.play_circle_outline
+              : Icons.stop_circle_outlined,
+          color: Colors.black45,
+        ),
+        backgroundColor: Colors.white60,
+        onPressed: () {
+          ttsState == TtsState.stopped ? _speak() : _stop();
+        },
       ),
     );
   }
