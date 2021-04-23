231c259c57f24621ec8467d6ac035dc7c5556913
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-04-23 23:16:05 +0100

    called custom theme function

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:note_app/const_values.dart';
+import 'package:note_app/custom_theme.dart';
 import 'package:note_app/screens/home.dart';
 
 class App extends StatefulWidget {
@@ -14,34 +13,7 @@ class _AppState extends State<App> {
     // final NoteModel changeTheme = Hive.box(noteBox).get(appHiveKey);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        appBarTheme: AppBarTheme(
-          centerTitle: true,
-          elevation: 0.0,
-          shadowColor: Colors.transparent,
-          color: Colors.transparent,
-          textTheme: TextTheme(
-            headline6: TextStyle(
-              color: Colors.black54,
-              fontSize: 25.0,
-            ),
-          ),
-          iconTheme: IconThemeData(
-            color: Colors.black54,
-          ),
-        ),
-        dialogBackgroundColor: backColor,
-        dialogTheme: DialogTheme(
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-          contentTextStyle: TextStyle(
-            color: Colors.black,
-          ),
-        ),
-      ),
+      theme: buildLightTheme(),
       title: 'VNotes',
       home: Home(),
     );
