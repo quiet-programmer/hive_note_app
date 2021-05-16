d7b3e6e30b68314d39843dd6fdd24d2317a8b37d
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-05-16 21:01:49 +0100

    added provider scopes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
 import 'package:note_app/const_values.dart';
 import 'package:note_app/models/note_model.dart';
+import 'package:note_app/providers/theme_provider.dart';
+import 'package:note_app/services/auth.dart';
 import 'package:note_app/src/app.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   Directory document = await getApplicationDocumentsDirectory();
   Hive
     ..init(document.path)
@@ -21,5 +26,18 @@ void main() async {
     DeviceOrientation.portraitDown,
   ]);
 
-  runApp(App());
+  runApp(
+    MultiProvider(
+      providers: [
+        StreamProvider.value(
+          value: AuthService().user,
+          initialData: null,
+        ),
+        ChangeNotifierProvider.value(
+          value: ThemeProvider(),
+        ),
+      ],
+      child: App(),
+    ),
+  );
 }
