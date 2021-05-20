4d273f84d7dc792fa71d326c8fad92794363efcd
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-05-20 19:54:10 +0100

    dynamic theme change

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/custom_theme.dart';
+import 'package:note_app/providers/theme_provider.dart';
 import 'package:note_app/screens/home.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatefulWidget {
   @override
@@ -10,10 +12,11 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
+    final checkTheme = Provider.of<ThemeProvider>(context);
     // final NoteModel changeTheme = Hive.box(noteBox).get(appHiveKey);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: buildLightTheme(),
+      theme: checkTheme.mTheme == false ? buildLightTheme() : buildDarkTheme(),
       title: 'VNotes',
       home: Home(),
     );
