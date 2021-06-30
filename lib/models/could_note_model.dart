429d6af0942132619b7f85131a9a00c12ce4d6d8
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-06-30 22:00:40 +0100

    thinking of restructing the database

--- lib/models/could_note_model.dart
+++ lib/models/could_note_model.dart
@@ -2,10 +2,12 @@ class CloudNoteModel {
   final String id;
   final String title;
   final String noteBody;
+  final DateTime timeAdded;
 
   CloudNoteModel({
     this.id,
     this.title,
     this.noteBody,
+    this.timeAdded,
   });
 }
