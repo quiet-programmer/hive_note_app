39dcc124bf3ea5e2eea6c49cc0b0ba85e87c6ae9
Godsend Joseph <bobbyjoseph108@gmail.com>
2022-08-26 19:33:10 +0100

    just some changes

--- lib/app/src/app.dart
+++ lib/app/src/app.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:note_app/app/screens/home.dart';
 import 'package:note_app/custom_theme.dart';
 import 'package:note_app/providers/theme_provider.dart';
 import 'package:provider/provider.dart';
 
 class App extends StatefulWidget {
-  const App({Key? key}) :super(key: key);
+  const App({Key? key}) : super(key: key);
+
   @override
   _AppState createState() => _AppState();
 }
@@ -14,12 +16,17 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     final checkTheme = Provider.of<ThemeProvider>(context);
-    // final NoteModel changeTheme = Hive.box(noteBox).get(appHiveKey);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: checkTheme.mTheme == false ? buildLightTheme() : buildDarkTheme(),
-      title: 'VNotes',
-      home: const Home(),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme:
+              checkTheme.mTheme == false ? buildLightTheme() : buildDarkTheme(),
+          title: 'VNotes',
+          home: const HomeScreen(),
+        );
+      },
     );
   }
 }
